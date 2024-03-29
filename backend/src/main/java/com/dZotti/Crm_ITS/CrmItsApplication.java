993b1eae5acb053baaf8c6e04195eabe67f82708
Daniel Zotti <daniel.zotti@linksmt.it>
2024-03-29 10:02:42 +0100

    Creati folder backend e frontend; Iniziallizato backend e frontend

---
+++ backend/src/main/java/com/dZotti/Crm_ITS/CrmItsApplication.java
@@ -0,0 +1,13 @@
+package com.dZotti.Crm_ITS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmItsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmItsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dZotti/Crm_ITS/CrmItsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dZotti.Crm_ITS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmItsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
